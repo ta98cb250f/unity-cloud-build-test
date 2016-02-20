a6c04c1e7cdb4b92dce4fa623afe821ce68c0177
ta98cb250f <ta98cb250f@gmail.com>
2016-02-20 23:07:26 +0900

    unity base setup

---
+++ Program/Assets/Scripts/Rotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	Transform _transform = null;
+
+	// Use this for initialization
+	void Start () {
+		_transform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_transform.Rotate( Time.deltaTime * 360.0f * Vector3.up );
+	}
+}
